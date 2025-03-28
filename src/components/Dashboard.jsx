5345c7e4a0453e439e5fe75246f0012c1312ff11
Mohammedshammas28 <m.shammas2k6@gmail.com>
2025-03-28 09:19:14 +0530

    Completed the assignment

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -17,7 +17,7 @@ const Dashboard = () => {
       genre: 'Science Fiction',
       releaseYear: 2010,
       synopsis: 'A thief who steals corporate secrets through dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.',
-      posterUrl: 'https://image.tmdb.org/t/p/original/xymM5aW6MDcH5AR9I3CamSegJd6.jpgg',
+      posterUrl: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_FMjpg_UX1000_.jpg',
     },
     {
       id: 2,
@@ -26,7 +26,7 @@ const Dashboard = () => {
       genre: 'Action',
       releaseYear: 1999,
       synopsis: 'A computer hacker learns about the true nature of his reality and his role in the war against its controllers.',
-      posterUrl: 'https://image.tmdb.org/t/p/original/dXNAPwY7VrqMAo51EKhhCJfaGb5.jpg',
+      posterUrl: 'https://m.media-amazon.com/images/M/MV5BN2NmN2VhMTQtMDNiOS00NDlhLTliMjgtODE2ZTY0ODQyNDRhXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
     },
     // Add more movie objects as needed
   ]);
